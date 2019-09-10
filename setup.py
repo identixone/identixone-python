e0f8d0d4242662ce9b2d29eecc4888b776d99317
Lev Rubel <l@datacorp.ee>
2019-09-10 18:55:47 +0300

    upped version

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/identixone/identixone-python',
-    version='0.1.6.1',
+    version='0.1.6.2',
     zip_safe=False,
 )
